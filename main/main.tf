fad35a6f57036691ed094b7deaff600e3c5bb978
Gajendra D Ambi <gajuambi@gmail.com>
2018-10-09 14:05:46 +0530

    '

--- main/main.tf
+++ main/main.tf
@@ -1,4 +1,7 @@
 
+
+
+variable "VAULT" {}
 resource "random_string" "monitoring_pass" {
   length = 30
   override_special = "!@#$%&*()-_=+[]{}<>:?" # excluding & to avoid problems with sed
@@ -7,6 +10,12 @@ resource "random_string" "monitoring_pass" {
   min_numeric = 8
   min_special = 7
 }
+
+# OUTPUTS
 output "monitoring_user_password" {
   value =  "${random_string.monitoring_pass.result}"
+}
+
+output "VAULT" {
+  value =  "${var.VAULT}"
 }
